c6bf2330d425ad1cecf4ff3680d0be233d86c2a0
Xasanovodil <xasanovodil43@gmail.com>
2026-06-23 11:17:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ─── Fill these in with YOUR Supabase project values ───────────────────────────
 // Supabase dashboard → Project Settings → API
-export const SUPABASE_URL = "https://xtlhmgfwkqxswxmbgtrz.supabase.co/rest/v1/";
+export const SUPABASE_URL = "https://xtlhmgfwkqxswxmbgtrz.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0bGhtZ2Z3a3F4c3d4bWJndHJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNDUzOTIsImV4cCI6MjA5NzcyMTM5Mn0.9Kx6TY7mucdWaZXzsITvL3X3ox7CoAlDQGv_0oDG8Ec";
 
 // The exam you're counting down to. Edit to your exact registered date & time
